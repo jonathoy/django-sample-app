9c69b6c0ee220874c04a55691d1138bf06c5228f
Jon Yu <jonathoy@umich.edu>
2019-04-24 12:59:14 -0400

    Add mentor model

--- mentorfinder/models.py
+++ mentorfinder/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Mentor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField
+    phone = models.PositiveSmallIntegerField
+    available = models.BooleanField
