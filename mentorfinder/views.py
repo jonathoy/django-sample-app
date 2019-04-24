0ea04e189ec4088f14b14cef8c7d82020a7079cf
Jon Yu <jonathoy@umich.edu>
2019-04-24 12:43:42 -0400

    Stub out create form, format views

--- mentorfinder/views.py
+++ mentorfinder/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mentorfinder/index.html')
+
+def create(request):
+    return render(request, 'mentorfinder/create.html')
